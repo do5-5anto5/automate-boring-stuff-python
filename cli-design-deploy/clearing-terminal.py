0355c19fc3635777da94fd01a59d2206cbe0ae8c
Jonathan <dos.santos.2k23@gmail.com>
2026-04-07 14:27:12 -0300

    Merge branch 'CLI' of https://github.com/do5-5anto5/automate-boring-stuff-python into CLI

--- cli-design-deploy/clearing-terminal.py
+++ cli-design-deploy/clearing-terminal.py
@@ -4,7 +4,7 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear') # soft deprecated
 
 
-import subprocess # recomended
+import subprocess # recomended simple to windows usage
 
 def clear_with_subprocess():
         subprocess.run(['cls'], shell = True)
