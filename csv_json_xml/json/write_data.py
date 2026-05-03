fd89586506476d20ba29d98a32257fce9158eef8
do5-5anto5 <dos.santos.2k23@gmail.com>
2026-05-03 11:07:09 -0300

    write and read csv files

---
+++ csv_json_xml/json/write_data.py
@@ -0,0 +1,19 @@
+import json
+
+python_dict = {
+    "name": "Alice Doe",
+    "age": 30,
+    "car": None,
+    "programmer": True,
+    "address": {"street": "100 Larkin St.", "city": "San Francisco", "zip": "94102"},
+    "phone": [
+        {"type": "mobile", "number": "415-555-7890"},
+        {"type": "work", "number": "415-555-1234"},
+    ],
+}
+
+# parse Python string data to json data
+json_string = json.dumps(python_dict, indent=2)
+
+# printing string
+print(json_string)
