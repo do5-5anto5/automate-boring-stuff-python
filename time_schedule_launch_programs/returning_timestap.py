e7d7fa429b7c3d9fdc5f8111194730d339c869cf
do5-5anto5 <dos.santos.2k23@gmail.com>
2026-05-05 20:25:24 -0300

    getting timestamp from currentime in seconds, and in descriptive string

---
+++ time_schedule_launch_programs/returning_timestap.py
@@ -0,0 +1,10 @@
+import time
+
+# getting timestamp in seconds sice 1/January/1970
+tstp = time.time()
+print(tstp)
+
+# getting a descriptive string of current time
+tstp = time.ctime()
+
+print(tstp)
